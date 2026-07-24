9594707ac9707adf59df56abfad9f134f74581a4
utigernils <utigernils@gmail.com>
2026-07-24 19:31:55 +0200

    fix; make sure led is off after blink

--- Firmware/SuvaSense_Firmware/sys_wifi.cpp
+++ Firmware/SuvaSense_Firmware/sys_wifi.cpp
@@ -1,10 +1,24 @@
 #include "sys_wifi.h"
 #include "sys_storage_wifi.h"
 #include "sys_serial.h"
+#include "hal_LED.h"
 #include <WiFi.h>
+
+extern LEDController leds;
 
 static unsigned long _lastReconnectAttempt = 0;
 static bool _connecting = false;
+static bool _wasConnected = false;
+static uint8_t _blinkRemaining = 0;
+static CRGB _blinkColor = CRGB::Black;
+static unsigned long _blinkLastToggle = 0;
+
+static void _startBlink(CRGB color) {
+  _blinkRemaining = 4;
+  _blinkColor = color;
+  _blinkLastToggle = millis();
+  leds.setUserColor(color);
+}
 
 void SysWiFi::setup() {
   String ssid     = StorageWiFi::getSSID();
@@ -25,15 +39,40 @@ void SysWiFi::setup() {
 }
 
 void SysWiFi::loop() {
-  if (WiFi.status() == WL_CONNECTED) {
+  bool connected = (WiFi.status() == WL_CONNECTED);
+
+  if (connected) {
     if (_connecting) {
       _connecting = false;
       SerialJSON::sendInfo("WiFi: connected, IP " + WiFi.localIP().toString());
     }
+    if (!_wasConnected) {
+      _startBlink(CRGB::Green);
+    }
+  } else {
+    if (_wasConnected) {
+      _startBlink(CRGB::Red);
+    }
+  }
+
+  _wasConnected = connected;
+
+  if (_blinkRemaining > 0) {
+    if (millis() - _blinkLastToggle >= 100) {
+      _blinkLastToggle = millis();
+      _blinkRemaining--;
+      if (_blinkRemaining % 2 == 0) {
+        leds.setUserColor(_blinkColor);
+      } else {
+        leds.setUserColor(CRGB::Black);
+      }
+    }
     return;
   }
 
-  if (!_connecting && millis() - _lastReconnectAttempt > 10000) {
+  leds.setUserColor(CRGB::Black);
+
+  if (!connected && !_connecting && millis() - _lastReconnectAttempt > 10000) {
     _lastReconnectAttempt = millis();
     SerialJSON::sendInfo("WiFi: reconnecting...");
     WiFi.reconnect();
