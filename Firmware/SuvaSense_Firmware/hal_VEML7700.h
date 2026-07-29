568205fd28979b44a6c8af1fb6be20ad5918ad41
Nils Utiger <nils.utiger@suva.ch>
2026-07-29 12:00:52 +0200

    feat; add VEML selftest

--- Firmware/SuvaSense_Firmware/hal_VEML7700.h
+++ Firmware/SuvaSense_Firmware/hal_VEML7700.h
@@ -9,11 +9,21 @@ struct VEML7700Data {
   float white;
 };
 
+#ifndef HAL_SELFTEST_RESULT
+#define HAL_SELFTEST_RESULT
+struct SelfTestResult {
+  String name;
+  bool ok;
+  String message;
+};
+#endif
+
 class VEML7700Sensor {
 public:
   VEML7700Sensor(TwoWire &wire = Wire);
   bool begin();
   VEML7700Data read();
+  SelfTestResult selfTest();
 
 private:
   Adafruit_VEML7700 _veml;
